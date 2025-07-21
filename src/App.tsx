72c0deedf6687789ec797b8f2c4e4dcac1759e24
amy <amybethdyson@gmail.com>
2025-07-21 11:04:07 -0700

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box>
-        <Router>
+        <Router basename="/sem-new">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
