b8096036e8786346ba023c95ab1a169331e24cc3
amy <amybethdyson@gmail.com>
2025-07-21 11:20:47 -0700

    add enviroment variable for redirect

--- src/main.tsx
+++ src/main.tsx
@@ -10,8 +10,9 @@ createRoot(document.getElementById("root")!).render(
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        redirect_uri: window.location.origin,
+        redirect_uri: window.location.origin + window.location.pathname,
       }}
+      cacheLocation="localstorage"
     >
       <App />
     </Auth0Provider>
